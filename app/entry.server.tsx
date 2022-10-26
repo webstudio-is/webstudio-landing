94f57d18ce3327b44b371d5419c8bfa289778768
Oleg Isonen <oleg008@gmail.com>
2022-10-27 01:53:35 +0200

    Revert "Revert "update packages and data""
    This reverts commit 43f61758e5d141deeca2800bf91d4dcc318fd4f4.

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,26 +1,2 @@
-import { renderToString } from "react-dom/server";
-import { RemixServer } from "remix";
-import type { EntryContext } from "remix";
-import { insertCriticalCss } from "@webstudio-is/sdk";
-
-export default function handleRequest(
-  request: Request,
-  responseStatusCode: number,
-  responseHeaders: Headers,
-  remixContext: EntryContext
-) {
-  let markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
-  );
-
-  // @webstudio
-  // Inline critical CSS as a style tag for maximum performance.
-  markup = insertCriticalCss(markup);
-
-  responseHeaders.set("Content-Type", "text/html");
-
-  return new Response("<!DOCTYPE html>" + markup, {
-    status: responseStatusCode,
-    headers: responseHeaders,
-  });
-}
+import { handleRequest } from "@webstudio-is/react-sdk";
+export default handleRequest;
